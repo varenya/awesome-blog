ecd52b37111dc223d1f6658e141b55e21e403c62
Varenya Thyagaraj <varen90@gmail.com>
2024-02-22 11:25:42 +0530

    format files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,70 @@
 import { createNewPost } from "@/app/actions";
 export default function Home() {
-	return (
-		<div className={"mx-auto flex-1"}>
-			<form className={"w-full"} action={createNewPost}>
-				<div className="space-y-12">
-					<div className="border-b border-gray-900/10 pb-12">
-						<h2 className="text-2xl font-semibold leading-7 text-gray-900 font-nunito uppercase">
-							Create a blog post
-						</h2>
+  return (
+    <div className={"mx-auto flex-1"}>
+      <form className={"w-full"} action={createNewPost}>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-2xl font-semibold leading-7 text-gray-900 font-nunito uppercase">
+              Create a blog post
+            </h2>
 
-						<div className="mt-10 ">
-							<div className="">
-								<label
-									htmlFor="title"
-									className="block text-sm font-medium leading-6 text-gray-900"
-								>
-									Title
-								</label>
-								<div className="mt-2 mb-4">
-									<div className="">
-										<input
-											type="text"
-											name="title"
-											id="title"
-											autoComplete="title"
-											className="h-12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-full"
-											placeholder="awesome blog"
-										/>
-									</div>
-								</div>
-							</div>
+            <div className="mt-10 ">
+              <div className="">
+                <label
+                  htmlFor="title"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Title
+                </label>
+                <div className="mt-2 mb-4">
+                  <div className="">
+                    <input
+                      type="text"
+                      name="title"
+                      id="title"
+                      autoComplete="title"
+                      className="h-12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-full"
+                      placeholder="awesome blog"
+                    />
+                  </div>
+                </div>
+              </div>
 
-							<div className="">
-								<label
-									htmlFor="content"
-									className="block text-sm font-medium leading-6 text-gray-900"
-								>
-									Content
-								</label>
-								<div className="mt-2">
-									<textarea
-										id="content"
-										name="content"
-										rows={4}
-										className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-full"
-									></textarea>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="mt-6 flex items-center justify-end gap-x-6">
-					<button
-						type="button"
-						className="text-sm font-semibold leading-6 text-gray-900"
-					>
-						Cancel
-					</button>
-					<button
-						type="submit"
-						className="bg-rose-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-10 rounded-lg w-full flex items-center justify-center sm:w-auto"
-					>
-						Save
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+              <div className="">
+                <label
+                  htmlFor="content"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Content
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="content"
+                    name="content"
+                    rows={4}
+                    className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-full"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="button"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="bg-rose-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-10 rounded-lg w-full flex items-center justify-center sm:w-auto"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,34 +4,34 @@ import { notFound } from "next/navigation";
 import { cookies } from "next/headers";
 
 export async function Navigation() {
-	const cookieStore = cookies();
-	const userId = cookieStore.get("userId") || { value: "1" };
-	const user = await prisma.user.findUnique({
-		where: { id: parseInt(userId.value, 10) },
-		include: {
-			posts: true,
-		},
-	});
-	if (!user) {
-		notFound();
-	}
-	const { posts } = user;
-	return (
-		<nav className="flex flex-1 flex-col" aria-label="Sidebar">
-			<h2 className="text-2xl font-semibold  text-gray-900 font-nunito mb-10 uppercase">
-				My blogs
-			</h2>
-			<ul role="list" className="m-0 ">
-				{posts.map((item) => (
-					<li key={item.id} className="mb-6">
-						<h3 className="text-xl font-semibold text-rose-600  text-gray-900 font-nunito">
-							{item.title}
-						</h3>
-						<p>{item.content}</p>
-						<ActiveLink href={`/post/${item.id}`}>Read more</ActiveLink>
-					</li>
-				))}
-			</ul>
-		</nav>
-	);
+  const cookieStore = cookies();
+  const userId = cookieStore.get("userId") || { value: "1" };
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(userId.value, 10) },
+    include: {
+      posts: true,
+    },
+  });
+  if (!user) {
+    notFound();
+  }
+  const { posts } = user;
+  return (
+    <nav className="flex flex-1 flex-col" aria-label="Sidebar">
+      <h2 className="text-2xl font-semibold  text-gray-900 font-nunito mb-10 uppercase">
+        My blogs
+      </h2>
+      <ul role="list" className="m-0 ">
+        {posts.map((item) => (
+          <li key={item.id} className="mb-6">
+            <h3 className="text-xl font-semibold text-rose-600  text-gray-900 font-nunito">
+              {item.title}
+            </h3>
+            <p>{item.content}</p>
+            <ActiveLink href={`/post/${item.id}`}>Read more</ActiveLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
