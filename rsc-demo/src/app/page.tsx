638fdb73c861ad24e88c2c7a400c11d74f95ea8f
varenya <vare90@gmail.com>
2024-02-24 09:24:26 +0530

    fix lint errors

--- rsc-demo/src/app/page.tsx
+++ rsc-demo/src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
                     name="content"
                     rows={4}
                     className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-full"
-                  ></textarea>
+                  />
                 </div>
               </div>
             </div>
